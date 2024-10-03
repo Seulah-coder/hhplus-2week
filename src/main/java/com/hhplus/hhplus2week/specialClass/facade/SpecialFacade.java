86625a0aa31e07a6d86aee8c0df353724685596b
seul <rser@hanmail.net>
2024-10-03 22:20:36 +0900

    special class 처리를 모은 facade

--- src/main/java/com/hhplus/hhplus2week/specialClass/facade/SpecialFacade.java
+++ src/main/java/com/hhplus/hhplus2week/specialClass/facade/SpecialFacade.java
@@ -1,15 +1,21 @@
 package com.hhplus.hhplus2week.specialClass.facade;
 
+import com.hhplus.hhplus2week.specialClass.HeadcountExceededException;
 import com.hhplus.hhplus2week.specialClass.application.SpecialClassInventoryService;
 import com.hhplus.hhplus2week.specialClass.application.SpecialClassItemService;
 import com.hhplus.hhplus2week.specialClass.application.SpecialClassService;
+import com.hhplus.hhplus2week.specialClass.application.SpecialEnrolmentService;
 import com.hhplus.hhplus2week.specialClass.domain.entity.SpecialClass;
 import com.hhplus.hhplus2week.specialClass.domain.entity.SpecialClassInventory;
 import com.hhplus.hhplus2week.specialClass.domain.entity.SpecialClassItem;
+import com.hhplus.hhplus2week.specialClass.domain.entity.SpecialEnrolment;
 import com.hhplus.hhplus2week.user.domain.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Component
 public class SpecialFacade {
@@ -22,11 +28,59 @@ public class SpecialFacade {
 
     @Autowired
     SpecialClassService specialClassService;
+    @Autowired
+    private SpecialEnrolmentService specialEnrolmentService;
 
-    public void saveAllSpecialClass(String name, String teacher) {
+    /**
+     * 초기 강의 등록 설정
+     * @param name
+     * @param teacher
+     * @param date
+     * @param openDate
+     * @return
+     */
+    public SpecialClassInventory saveInitAllSpecialClass(String name, String teacher ,LocalDateTime date, LocalDateTime openDate) {
+        long totalHeadcount = 30L;
         SpecialClass specialClass = specialClassService.saveSpecialClass(name, teacher);
-        SpecialClassItem specialClassItem = specialClassItemService.saveSpecialClassItem()
+        SpecialClassItem specialClassItem = specialClassItemService.saveSpecialClassItem(specialClass.getId(), date, totalHeadcount);
+        SpecialClassInventory specialClassInventory = specialClassInventoryService.saveSpecialClassInventory(specialClass.getId(), specialClassItem.getId(), 0);;
+        return specialClassInventory;
+    }
 
+    /**
+     * 강의 정원이 totalHeadcount 이하인지 체크한다
+     * @param itemId
+     * @return
+     */
+    public boolean checkHeadCount(long itemId){
+        SpecialClassItem item = specialClassItemService.getSpecialClassItem(itemId);
+        SpecialClassInventory inventory = specialClassInventoryService.getSpecialClassInventoryByItemId(itemId);
+        if(item.getTotalHeadcount() <= inventory.getCurrentHeadcount()){
+            throw new HeadcountExceededException("전체 정원 오버로 신청 불가");
+        }
+        return true;
+    }
+
+    /**
+     * 유저가 강의를 신청한다.
+     * @param userId
+     * @param itemId
+     * @return
+     */
+    public SpecialEnrolment saveUserSpecialClassEnrolment(long userId, long itemId){
+        SpecialEnrolment enrolment  = specialEnrolmentService.saveSpecialEnrolment(userId, itemId);
+        SpecialClassInventory inventory = specialClassInventoryService.getSpecialClassInventoryByItemId(itemId);
+        inventory.setCurrentHeadcount(inventory.getCurrentHeadcount() + 1);
+        specialClassInventoryService.updateSpecialClassInventory(inventory);
+        return enrolment;
+    }
+
+    /**
+     * 현재 날짜 이후로 신청 가능한 강의 목록을 조회한다.
+     * @return
+     */
+    public List<SpecialClassItem> getSpecialClassItemsAfterNow(){
+        return specialClassItemService.getSpecialClassItemsAfterNow();
     }
 
 
--- src/main/java/com/hhplus/hhplus2week/specialClass/facade/SpecialUserFacade.java
+++ src/main/java/com/hhplus/hhplus2week/specialClass/facade/SpecialUserFacade.java
@@ -1,7 +1,11 @@
 package com.hhplus.hhplus2week.specialClass.facade;
 
-import com.hhplus.hhplus2week.specialClass.application.SpecialClassService;
+import com.hhplus.hhplus2week.specialClass.application.SpecialClassInventoryService;
+import com.hhplus.hhplus2week.specialClass.application.SpecialEnrolmentService;
+import com.hhplus.hhplus2week.specialClass.domain.entity.SpecialEnrolment;
 import com.hhplus.hhplus2week.user.application.UserService;
+import com.hhplus.hhplus2week.user.domain.entity.User;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -9,9 +13,35 @@ import org.springframework.stereotype.Component;
 public class SpecialUserFacade {
 
     @Autowired
-    SpecialClassService specialClassService;
+    SpecialFacade specialFacade;
 
     @Autowired
     UserService userService;
 
+    @Autowired
+    SpecialEnrolmentService specialEnrolmentService;
+
+    /**
+     * 유저가 강의를 신청한다
+     * @param userId
+     * @param itemId
+     */
+    @Transactional
+    public void enrolSpecialClass(long userId, long itemId){
+        try {
+            User user = userService.getUser(userId);
+            if(user != null){
+                boolean checkResult = specialFacade.checkHeadCount(itemId);
+                if(checkResult){
+                    specialFacade.saveUserSpecialClassEnrolment(userId, itemId);
+                }
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public SpecialEnrolment getSpecialEnrolment(long userId){
+        return specialEnrolmentService.getSpecialEnrolment(userId);
+    }
 }
