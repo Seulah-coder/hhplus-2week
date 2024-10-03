6c8df0373fa5db73beffcb3b099a10740f411ff9
seul <rser@hanmail.net>
2024-10-03 22:19:02 +0900

    오늘 날짜 기준으로 신청 가능한 특강 조회

--- src/main/java/com/hhplus/hhplus2week/specialClass/persistence/SpecialClassItemRepository.java
+++ src/main/java/com/hhplus/hhplus2week/specialClass/persistence/SpecialClassItemRepository.java
@@ -2,11 +2,17 @@ package com.hhplus.hhplus2week.specialClass.persistence;
 
 import com.hhplus.hhplus2week.specialClass.domain.entity.SpecialClassItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface SpecialClassItemRepository extends JpaRepository<SpecialClassItem, Long> {
     List<SpecialClassItem> findAllByClassId(long classId);
+
+    @Query("SELECT i FROM SpecialClassItem i WHERE i.openDate > :date")
+    List<SpecialClassItem> findSpecialItemsAfterTheDate(@Param("date") LocalDateTime date);
 }
