89b455ad53fc48405be74f704cdeff9e74b4fbb3
seul <rser@hanmail.net>
2024-10-03 23:20:35 +0900

    API Specs 작성

--- src/main/java/com/hhplus/hhplus2week/specialClass/application/SpecialClassController.java
+++ src/main/java/com/hhplus/hhplus2week/specialClass/application/SpecialClassController.java
@@ -1,7 +1,75 @@
 package com.hhplus.hhplus2week.specialClass.application;
 
+import com.hhplus.hhplus2week.specialClass.domain.entity.SpecialClass;
+import com.hhplus.hhplus2week.specialClass.domain.entity.SpecialClassInventory;
+import com.hhplus.hhplus2week.specialClass.domain.entity.SpecialClassItem;
+import com.hhplus.hhplus2week.specialClass.domain.entity.SpecialEnrolment;
+import com.hhplus.hhplus2week.specialClass.facade.SpecialFacade;
+import com.hhplus.hhplus2week.specialClass.facade.SpecialUserFacade;
+import lombok.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+@RestController
+@RequestMapping("/specialClass")
 public class SpecialClassController {
+
+    private final SpecialFacade specialFacade;
+    private final SpecialUserFacade specialUserFacade;
+
+
+    public SpecialClassController(SpecialFacade specialFacade, SpecialUserFacade specialUserFacade) {
+        this.specialFacade = specialFacade;
+        this.specialUserFacade = specialUserFacade;
+    }
+
+    /**
+     * 특강 생성
+     * @param params
+     * @return
+     */
+    @PostMapping("/create")
+    public ResponseEntity<SpecialClassInventory> createSpecialClass(Map<String, Objects> params){
+        SpecialClassInventory inventory = specialFacade.saveInitAllSpecialClass(params.get("name").toString(), params.get("teacher").toString(), params.get("openDate").toString());
+        return ResponseEntity.ok(inventory);
+    }
+
+    /**
+     * 오늘 날짜 이후 특강 조회
+     * @return
+     */
+    @GetMapping("/list")
+    public ResponseEntity<List<SpecialClassItem>> getSpecialClassList(){
+        List<SpecialClassItem> itemList = specialFacade.getSpecialClassItemsAfterNow();
+        return ResponseEntity.ok(itemList);
+    }
+
+    /**
+     * 유저가 신청한 특강 조회
+     * @param userId
+     * @return
+     */
+    @GetMapping("/fetchlist/{id}")
+    public ResponseEntity<List<SpecialEnrolment>> getSpecialEnrolments(@PathVariable("id") long userId){
+        List<SpecialEnrolment> enrolments = specialUserFacade.getSpecialEnrolments(userId);
+        return ResponseEntity.ok(enrolments);
+    }
+
+    /**
+     * 특강 신청
+     * @param userId
+     * @param itemId
+     * @return
+     */
+    @GetMapping("/enrolUser/{userId}/{itemId}}")
+    public ResponseEntity<SpecialEnrolment> enrolSpecialClass(@PathVariable("userId")long userId, @PathVariable("itemId") long itemId){
+        SpecialEnrolment enrolment = specialFacade.saveUserSpecialClassEnrolment(userId, itemId);
+        return ResponseEntity.ok(enrolment);
+    };
 }
